c9d5d116f4625eb81aae51f0d32e3aa5a0d2fda5
Viorel Dumitrescu <vioreld@x4.(none)>
2013-07-07 11:22:42 -0400

    Increase buffer in web.js

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-var buffer=new Buffer(100)
+var buffer=new Buffer(10000)
 var fs=require('fs')
 var file = fs.openSync("index.html", "r")
-var len = fs.readSync(file, buffer, 0, 100, 0);
+var len = fs.readSync(file, buffer, 0, 10000, 0);
 //console.log(buffer.toString('utf8', 0, len))
 
 var express = require('express');
